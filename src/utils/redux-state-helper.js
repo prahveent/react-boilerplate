574d40b9edd004c88f654d8366c2cd3ecfaf2f59
prahveent <prahveent@gmail.com>
2020-01-25 15:29:35 +0530

    move to ts

--- src/utils/redux-state-helper.js
+++
@@ -1,6 +0,0 @@
-﻿export const updateObject = (oldObject, updatedProperties) => {
-    return {
-        ...oldObject,
-        ...updatedProperties
-    };
-};
---
+++ src/utils/redux-state-helper.ts
@@ -0,0 +1,6 @@
+﻿export const updateObject = (oldObject : Object, updatedProperties : Object) => {
+    return {
+        ...oldObject,
+        ...updatedProperties
+    };
+};
